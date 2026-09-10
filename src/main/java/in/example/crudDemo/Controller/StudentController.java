528458203f773f08a571fe91d3c26dbf3f06f8d4
Namdev Kadam <namdevkadam6266@gmail.com>
2026-09-10 12:13:50 +0530

    Initial commit

---
+++ src/main/java/in/example/crudDemo/Controller/StudentController.java
@@ -0,0 +1,54 @@
+package in.example.crudDemo.Controller;
+
+import in.example.crudDemo.entity.Student;
+import in.example.crudDemo.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/student") //Common end point
+public class StudentController {
+    //create student
+    private StudentService studentService;
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+     @PostMapping("/create")
+     public ResponseEntity<Student> createStudent(@RequestBody Student student){
+          Student createdzStudent =studentService.createStudent(student);
+         return ResponseEntity
+                 .status(HttpStatus.CREATED).body(createdzStudent);
+    }
+     //return
+    @GetMapping("/get/{id}")
+     public ResponseEntity<?> getStudent(@PathVariable Long id){
+      Student getStudent = studentService.getStudent(id);
+      if (getStudent==null){
+          return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Somthing went wrong");
+      }
+       return  ResponseEntity.status(HttpStatus.OK).body(getStudent);
+
+    };
+    //Get all student
+    @GetMapping("/getall")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> studentResp = studentService.GetAllStudents();
+        if (studentResp.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(studentResp);
+    };
+    //update
+    @PutMapping("/update/{id}")
+   public ResponseEntity<Student> updataStudent(@PathVariable long id,@RequestBody Student student){
+       Student studentResp = studentService.update(id,student);
+       if(studentResp==null){
+           return ResponseEntity.notFound().build();
+       }
+       return ResponseEntity.status(HttpStatus.OK).body(studentResp);
+   }
+    //delete
+}
---
+++ src/main/java/in/example/crudDemo/CrudDemoApplication.java
@@ -0,0 +1,14 @@
+package in.example.crudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+		System.out.println("Application started");
+	}
+}
